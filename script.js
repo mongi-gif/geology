ef9dc8f0408cdadbc266853dc17c5456baca28c1
mongi-gif <55959817+mongi-gif@users.noreply.github.com>
2026-07-01 10:59:30 +0100

    Update script.js

--- script.js
+++ script.js
@@ -26,31 +26,195 @@ const gastronomie = L.layerGroup().addTo(map);
 const sites = [
 
 {
-    nom:"Synclinal perché de Dyr El Kef",
-    type:"geologie",
-    lat:36.198,
-    lng:8.727,
-    couleur:"red",
-    url:"https://drive.google.com/file/d/13B__lgAuCyeWWLBnbSm5qsHI62rjniuN/view"
+    nom: "synclinal perché éocène de Dyr El Kef",
+    type: "geologie",
+    lat: 36.198,
+    lng: 8.727,
+    couleur: "red",
+ 
+    url: "https://drive.google.com/file/d/13B__lgAuCyeWWLBnbSm5qsHI62rjniuN/view?usp=sharing"
+},
+
+  
+{
+    nom: "Stratotype K-Pg du Kef",
+    type: "geologie",
+    lat: 36.153,
+    lng: 8.648,
+    couleur: "red",
+ 
+    url: "https://drive.google.com/file/d/13srHXLY15B5ANww84mHWlnBysQmzWccJ/view?usp=sharing"
+},
+{
+    nom: "Série triasque du Débadib",
+    type: "geologie",
+    lat: 36.175,
+    lng: 8.583,
+    couleur: "red",
+ 
+    url: "https://drive.google.com/file/d/1U8Xa0oAe19KB6up6lUMAzQFsg_iE1OZn/view?usp=sharing"
+},
+{
+    nom: "Grotte spléologique",
+    type: "geologie",
+    lat: 36.0299,
+    lng: 8.321,
+    couleur: "red",
+  
+    url: "https://drive.google.com/file/d/1XI48vuvIVFh9er21EKO03uZO_LbcqOqV/view?usp=sharing"
+},
+{
+    nom: "Niveau à Ammonites de l'Aptien Supérieur",
+    type: "geologie",
+    lat: 35.990,
+    lng: 8.350,
+    couleur: "red",
+  
+    url: "https://drive.google.com/file/d/13qjY9WSQljVTyOCmbYuz7d_NIxCRpHn_/view?usp=sharing"
+},
+{
+    nom: "récif aptien du jeble Slata",
+    type: "geologie",
+    lat: 35.873,
+    lng: 8.470,
+    couleur: "red",
+  
+    url: "https://drive.google.com/file/d/1AupexraKu_yGd5oGDqvwCfXi-C-MqfDc/view?usp=sharing"
 },
 
 {
-    nom:"Mine de Jerissa",
-    type:"minier",
-    lat:35.85,
-    lng:8.63,
-    couleur:"blue",
-    url:"https://drive.google.com/file/d/1LO2kPfHMkvd88d4mDUQsYsHZ2oQrECMa/view"
+    nom: "Mine de Jerissa",
+    type: "minier",
+    lat: 35.85,
+    lng: 8.63,
+    couleur: "blue",
+  
+    url: "https://drive.google.com/file/d/1LO2kPfHMkvd88d4mDUQsYsHZ2oQrECMa/view?usp=sharing"
+},
+{
+    nom: "Phosphate de Sra Ouertane",
+    type: "minier",
+    lat: 35.821,
+    lng: 8.767,
+    couleur: "blue",
+ 
+    url: "https://drive.google.com/file/d/1p8fox3qBTJnIh8c2px_pQ74Z7ltTDI7j/view?usp=sharing"
 },
 
 {
-    nom:"Circuit Médina du Kef",
-    type:"historique",
-    lat:36.18196,
-    lng:8.71269,
-    couleur:"orange",
-    url:"https://www.siccaveneria.com/"
-}
+    nom: "Bou Jabeur",
+    type: "minier",
+    lat: 35.737,
+    lng: 8.282,
+    couleur: "blue",
+
+    url: "https://drive.google.com/file/d/14mVyiJFh2MkW5Cc2nHHhr6nD6JRCOzZ2/view?usp=sharing"
+},
+
+{
+    nom: "Circuit de la Medina du Kef",
+    lat: 36.18196,
+    lng: 8.71269,
+    type: "historique",
+    couleur: "orange",
+    
+        url: "https://www.siccaveneria.com/tourisme/patrimoine/16d5854d-d83a-11ea-af16-0242ac11000b"
+
+  },
+  
+  {
+    nom: "Sidi Zine",
+    lat: 36.125,
+    lng: 8.634,
+    type: "historique",
+    couleur: "orange",
+ 
+    url: "https://tunisiaforall.org/?page_id=2493"
+
+  },
+  {
+    nom: "Althiburos",
+    lat: 35.879,
+    lng: 8.786,
+    type: "historique",
+     couleur: "orange",
+
+        url: "https://www.persee.fr/doc/dha_0755-7256_2021_num_47_1_5065"
+
+  },
+  
+  {
+    nom: "Hammam Mellegue",
+    lat: 36.183,
+    lng: 8.575,
+    type: "historique",
+    couleur: "orange",
+
+        url: "https://fr.aroundus.com/p/9636927-hammam-mellegue"
+
+  },
+  
+  {
+    nom: "table de Jughurta",
+    lat:35.746,
+    lng: 8.378,
+    couleur: "orange",
+  
+        url: "https://whc.unesco.org/fr/listesindicatives/6278/"
+
+  },
+
+  {
+    nom: "Elles",
+    lat:35.950,
+    lng: 9.097,
+    type: "historique",
+    couleur: "orange",
+
+           url: "https://fr.wikipedia.org/wiki/M%C3%A9galithes_d%27Ell%C3%A8s"
+
+  },
+  {
+    nom: "Réserve de saddine",
+    lat:35.950,
+    lng: 9.097,
+    type: "naturel",
+    couleur: "green",
+
+        url: "https://www.onagri.nat.tn/uploads/divers/atlas/RN_JebelSaddine.pdf"
+
+  },
+  {
+    nom: "Forêt de Nebeur",
+    lat:36.310,
+    lng: 8.694,
+    type: "naturel",
+    couleur: "green",
+ 
+        url: "https://fr.wikipedia.org/wiki/Barrage_Mell%C3%A8gue"
+
+  },
+  {
+    nom: "Gastronomie chez Chef Fatma",
+    lat:36.18097,
+    lng: 8.70821,
+    type: "gastronomie",
+    couleur:"#53EAFD",
+ 
+        url: "https://www.tripadvisor.fr/LocationPhotoDirectLink-g946559-d21312292-i484251395-Chef_Fatma-Le_Kef_Le_Kef_Governorate.html"
+
+  },
+
+  {
+    nom: "Chajret oum Chlalig",
+    lat:36.16858,
+    lng: 8.59243,
+    type: "ethno",
+    couleur: "maroon",
+  
+        url: "https://drive.google.com/file/d/1mMeImf0yFeDMPpfSxna01_LdPoyBw-uK/view?usp=sharing"
+
+  },
 
 ];
 
